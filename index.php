e2e89c88e127dba0782f1d3923559bcc4f408b58
Apisit_k <aew.apisit@gmail.com>
2019-09-04 14:29:22 +0700

    no message

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+
+// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'vendor/autoload.php';
 
 use LINE\LINEBot;
