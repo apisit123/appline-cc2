80fdf0c827a289501152bdc4170176ad8e3c0a17
Apisit_k <aew.apisit@gmail.com>
2019-09-04 11:16:01 +0700

    no message

--- index.php
+++ index.php
@@ -132,10 +132,6 @@ function link_richmenu($userID,$richID) {
 
 	curl_setopt_array($curl, array(
 	  CURLOPT_URL => "https://api.line.me/v2/bot/user/".$userID."/richmenu/".$richID,
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => "",
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 30,
 	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	  CURLOPT_CUSTOMREQUEST => "POST",
 	  CURLOPT_HTTPHEADER => array(
@@ -168,16 +164,12 @@ function post_line($userID, $msgtype, $data_post){
 
 	$data_post = json_encode($myObj);
 
-	error_log($data_post)
+	error_log($data_post);
 
     $curl = curl_init();
     
     curl_setopt_array($curl, array(
       CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
-      CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => "",
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
       CURLOPT_POSTFIELDS =>  $data_post,
