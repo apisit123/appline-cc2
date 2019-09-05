30c3296c21646bfb55c44e4d274b7aff2daaf97b
Apisit_k <aew.apisit@gmail.com>
2019-09-05 10:26:54 +0700

    no message

--- index.php
+++ index.php
@@ -55,7 +55,10 @@ use LINE\LINEBot\RichMenuBuilder\RichMenuSizeBuilder;
 use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBuilder;
 use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBoundsBuilder;
 
-include_once("setting.php");
+
+define('LINE_MESSAGE_CHANNEL_ID','1648723890');
+define('LINE_MESSAGE_CHANNEL_SECRET','1e61a9805bde296ff2b3b2fbbe302220');
+define('LINE_MESSAGE_ACCESS_TOKEN','ZJtS+TuOmS9FWEHc1Idm7RF2dXjszDOOWHDAUcx2LLMyQP9DStP4PUXeoQjX6gtdYRy9hBD5T5opbbeHMb5zjU3PonwLzSKCbkJ2CQ5scC7uTmojN2I38anReeBjmzdcKGl6sq5758gXHdB21DgeHlGUYhWQfeY8sLGRXgo3xvw=');
 
 
 
