37c163386b5a82a33af57c0ffb4cca7d8d818537
Apisit_k <aew.apisit@gmail.com>
2019-10-05 11:17:01 +0700

    time to commit

--- bot_action.php
+++ bot_action.php
@@ -494,8 +494,12 @@ if(!is_null($events)){
                                 new BoxComponentBuilder(
                                     "vertical",
                                     array(
-                                        new TextComponentBuilder("This is Body")
-                                    )
+                                      new ImageComponentBuilder(
+                                        //url, flex, margin, align, gravity, size, aspecratio, aspecmode, backgroud, action, position, offsettop, offsetbottom, offsetstart, offsetend
+                                        "https://appline.cctfts.com/tfts/img/toofast_order_logo.jpg",NULL,NULL,NULL,NULL,"full","20:13","cover",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"100px","72px","72px")
+                                      // text, flex, margin, size, align. gravity, warp, maxline, weight, color, action
+                                      new TextComponentBuilder("Queue SIAM",NULL,NULL,"xxl","center","center")
+                                      )
                                 ),
                                 new BoxComponentBuilder(
                                     "vertical",
