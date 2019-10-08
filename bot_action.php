a562603b2cbad7d60a79f9523b407ac9bf6f1383
Apisit_k <aew.apisit@gmail.com>
2019-10-08 13:31:32 +0700

    no message

--- bot_action.php
+++ bot_action.php
@@ -505,6 +505,29 @@ if(!is_null($events)){
 
                           break;
 
+
+                          case (preg_match('/(สั่ง)/', $userMessage) ? true : false):
+
+                            $imageMapUrl = 'https://storage.googleapis.com/toofast-bucket/linebot/how_to_order.png?_ignored=';
+                            $replyData = new MultiMessageBuilder();
+                            
+
+                            $replyData -> add(new ImagemapMessageBuilder(
+                                $imageMapUrl,
+                                'HOW TO ORDER',
+                                new BaseSizeBuilder(1486,1040),
+                                array(
+                                  new ImagemapMessageActionBuilder(
+                                      '...',
+                                      new AreaBuilder(0,0,1,1)
+                                      )
+                                )
+                              ))
+
+                                        -> add(new VideoMessageBuilder("https://storage.googleapis.com/toofast-bucket/video%20cc2/ccamazon.mp4", "https://storage.googleapis.com/toofast-bucket/video%20cc2/ccamazon_Moment.jpg"));
+
+                        break;
+
 /**     
   public function __construct(
       $text,
