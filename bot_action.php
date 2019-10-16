f698c6455a70de6016d1dc34fba91ffa3d6cd15a
Apisit_k <aew.apisit@gmail.com>
2019-10-16 09:45:43 +0700

    no message

--- bot_action.php
+++ bot_action.php
@@ -693,30 +693,28 @@ if(!is_null($events)){
                         $replyData  -> add(new TextMessageBuilder("Line@ Too Fast Order เป็นระบบอัตโนมัติ"))
                                     -> add(new TextMessageBuilder("หากลูกค้ามีข้อสงสัยเพิ่มเติม กรุณาติดต่อที่ Cashier แต่ละสาขานะคะ ^^"));
 
-                        $url = "https://bots.dialogflow.com/line/80a5e0d6-016f-46f5-b8b8-0302c02896b3/webhook";
-                        $headers = getallheaders();
-            //          file_put_contents('headers.txt',json_encode($headers, JSON_PRETTY_PRINT));          
-            //          file_put_contents('body.txt',file_get_contents('php://input'));
-                        $headers['Host'] = "bots.dialogflow.com";
-                        $json_headers = array();
-                        foreach($headers as $k=>$v){
-                            $json_headers[]=$k.":".$v;
-                        }
-                        $inputJSON = file_get_contents('php://input');
-                        // // ส่วนของการส่งการแจ้งเตือนผ่านฟังก์ชั่น cURL
-                        $ch = curl_init();
-                        curl_setopt( $ch, CURLOPT_URL, $url);
-                        curl_setopt( $ch, CURLOPT_POST, 1);
-                        curl_setopt( $ch, CURLOPT_BINARYTRANSFER, true);
-                        curl_setopt( $ch, CURLOPT_POSTFIELDS, $inputJSON);
-                        curl_setopt( $ch, CURLOPT_HTTPHEADER, $json_headers);
-                        curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2); // 0 | 2 ถ้าเว็บเรามี ssl สามารถเปลี่ยนเป้น 2
-                        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1); // 0 | 1 ถ้าเว็บเรามี ssl สามารถเปลี่ยนเป้น 1
-                        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-                        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-                        $result = curl_exec( $ch );
-                        curl_close( $ch );
-                        exit;
+                        // $url = "https://bots.dialogflow.com/line/80a5e0d6-016f-46f5-b8b8-0302c02896b3/webhook";
+                        // $headers = getallheaders();
+                        // $headers['Host'] = "bots.dialogflow.com";
+                        // $json_headers = array();
+                        // foreach($headers as $k=>$v){
+                        //     $json_headers[]=$k.":".$v;
+                        // }
+                        // $inputJSON = file_get_contents('php://input');
+                        // // // ส่วนของการส่งการแจ้งเตือนผ่านฟังก์ชั่น cURL
+                        // $ch = curl_init();
+                        // curl_setopt( $ch, CURLOPT_URL, $url);
+                        // curl_setopt( $ch, CURLOPT_POST, 1);
+                        // curl_setopt( $ch, CURLOPT_BINARYTRANSFER, true);
+                        // curl_setopt( $ch, CURLOPT_POSTFIELDS, $inputJSON);
+                        // curl_setopt( $ch, CURLOPT_HTTPHEADER, $json_headers);
+                        // curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2); // 0 | 2 ถ้าเว็บเรามี ssl สามารถเปลี่ยนเป้น 2
+                        // curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1); // 0 | 1 ถ้าเว็บเรามี ssl สามารถเปลี่ยนเป้น 1
+                        // curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+                        // curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+                        // $result = curl_exec( $ch );
+                        // curl_close( $ch );
+                        // exit;
 
                         break;                                      
                 }
