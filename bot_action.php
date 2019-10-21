082a46cce8a1dbe5f9a2cc78f13a2e5b3f4af718
Apisit_k <aew.apisit@gmail.com>
2019-10-21 11:17:39 +0700

    no message

--- bot_action.php
+++ bot_action.php
@@ -539,14 +539,19 @@ if(!is_null($events)){
 
 
                           case "reciept":
+                            $array = {"Volvo", "BMW", "Ford", "Mazda"};
 
-                            $txt = new BoxComponentBuilder(
-                                            "horizontal",
-                                            array(
-                                                new TextComponentBuilder("Chewing Gum", 0, NULL, "sm", NULL, NULL, NULL, NULL, NULL,"#555555"),
-                                                new TextComponentBuilder("$0.99", NULL, NULL, "sm", "end", NULL, NULL, NULL, NULL,"#111111")
-                                            )
-                                        );
+                            foreach ( $array as $value ) {
+
+                                $txt[] = new BoxComponentBuilder(
+                                        "horizontal",
+                                        array(
+                                            new TextComponentBuilder($value, 0, NULL, "sm", NULL, NULL, NULL, NULL, NULL,"#555555"),
+                                            new TextComponentBuilder("$0.99", NULL, NULL, "sm", "end", NULL, NULL, NULL, NULL,"#111111")
+                                        )
+                                    );
+                            }
+                            
 
                             $textReplyMessage = new BubbleContainerBuilder(
                                 "ltr",NULL,NULL,
