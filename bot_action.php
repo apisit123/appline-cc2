c30089587df8d9e2cb94a802de625a850658d097
Apisit_k <aew.apisit@gmail.com>
2019-10-21 11:01:39 +0700

    no message

--- bot_action.php
+++ bot_action.php
@@ -558,16 +558,13 @@ if(!is_null($events)){
                                  new BoxComponentBuilder(
                                     "vertical",
                                     array(
-                                        foreach($array as $value) {
-                                            new BoxComponentBuilder(
-                                                "horizontal",
-                                                array(
-                                                    new TextComponentBuilder($value, 0, NULL, "sm", NULL, NULL, NULL, NULL, NULL,"#555555"),
-                                                    new TextComponentBuilder("$0.99", NULL, NULL, "sm", "end", NULL, NULL, NULL, NULL,"#111111")
-                                                )
-                                            ),
-                                        }
-                                        
+                                        new BoxComponentBuilder(
+                                            "horizontal",
+                                            array(
+                                                new TextComponentBuilder("Chewing Gum", 0, NULL, "sm", NULL, NULL, NULL, NULL, NULL,"#555555"),
+                                                new TextComponentBuilder("$0.99", NULL, NULL, "sm", "end", NULL, NULL, NULL, NULL,"#111111")
+                                            )
+                                        ),
                                     ),
                                     NULL,
                                     "sm",
